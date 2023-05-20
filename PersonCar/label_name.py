19ffc69a3e4dc58d649b13cd816913e633fd8852
fadai-0110 <94634319+fadai-0110@users.noreply.github.com>
2023-05-20 19:28:55 +0800

    changedata

---
+++ PersonCar/label_name.py
@@ -0,0 +1,12 @@
+# -*- coding: GBK -*-
+"""
+@Time ： 2022/6/24 15:31
+@Auth ： 思绪无限
+Bilibili：思绪亦无限
+公众号：AI技术研究与分享
+@File ：bird_name.py
+@IDE ：PyCharm
+功能：
+"""
+Chinese_name = {'person': "行人", 'bicycle': '自行车', 'car': '小汽车', 'motorcycle': '摩托车',
+                'bus': '公交车', 'truck': '卡车', 'train': '火车'}
